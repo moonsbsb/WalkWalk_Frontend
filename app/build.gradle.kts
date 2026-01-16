f2ab41af2f5b2465fecfe4eabe0496ec895b337b
moon890309 <abcdefg8989@naver.com>
2026-01-16 20:09:40 +0900

    fix: 상태바 색상 변경 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.withwalk.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = secret.getProperty("KAKAO_NATIVE_APP_KEY") ?: ""
